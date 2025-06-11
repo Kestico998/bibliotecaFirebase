ba47bfc39f3f98342301b284f1608bdbe82d481e
Roman <jorgeromanazuaravillafuerte@gmail.com>
2025-06-11 15:20:59 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
  const firebaseConfig = {
-    apiKey: "AIzaSyBTU",
-    authDomain: "bdnosql-de619.firebaseapp.com",
-    projectId: "b19",
-    storageBucket: "bdnoapp",
-    messagingSenderId: "519",
-    appId: "1:5193611",
-    measurementId: "G0"
+    apiKey: "AIzaSyDAh5AoZhM02GZrEXn6rzQBCXwZZ89zCHU",
+    authDomain: "bdnosql-59def.firebaseapp.com",
+    projectId: "bdnosql-59def",
+    storageBucket: "bdnosql-59def.firebasestorage.app",
+    messagingSenderId: "693412492447",
+    appId: "1:693412492447:web:0204d931558e9be932deaa",
+    measurementId: "G-N1RYK4YXBK"
   };
 
 const app = initializeApp(firebaseConfig);
